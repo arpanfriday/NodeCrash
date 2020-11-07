14e77103423bb707868b93df838a9dd35a11b540
binary-greens <arpanriju2000@gmail.com>
2020-11-07 16:30:27 +0530

    initial

---
+++ index.js
@@ -0,0 +1,5 @@
+const Person = require('./person');
+
+
+const Person1 = new Person("Arpan", 12);
+Person1.display();
---
+++ person.js
@@ -0,0 +1,20 @@
+// Module Wrapper Function
+// function(exports, require, module, __filename, __dirname) {
+
+// }
+
+console.log(__dirname);
+console.log(__filename);
+
+class Person {
+    constructor(name, age){
+        this.name = name;
+        this.age = age;
+    }
+
+    display() {
+        console.log(`My name is ${this.name} and my age is ${this.age}`);
+    }
+}
+
+module.exports = Person;
